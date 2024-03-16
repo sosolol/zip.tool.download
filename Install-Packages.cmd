45d5d52f3346512d5f075503ec6dd654941c3fdb
soso <127365917+sosolol@users.noreply.github.com>
2024-03-16 14:26:13 -0400

    Add files via upload

---
+++ Install-Packages.cmd
@@ -0,0 +1,15 @@
+cls && npm i gradient-string
+cls && npm i node-fetch@2.6.12
+cls && npm i node-bash-title
+cls && npm i request
+cls && npm i fs-extra
+cls && npm i axios
+cls && npm i cheerio
+cls && npm i soundcloud-downloader
+cls && npm i fluent-ffmpeg
+cls && npm i @ffmpeg-installer/ffmpeg
+cls && npm i discord-rpc
+cls && npm i boxen@5.1.2
+cls && npm i node-macaddress
+cls && npm i readline-sync
+
